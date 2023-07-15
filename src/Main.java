af46461dc3be6ca07eb874474685ed598cbec26a
Alessandro <alex97av@gmail.com>
2023-07-16 00:03:18 +0200

    creazione scanner e ciclo while

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner inValue = new Scanner(System.in);
+        System.out.print("Please enter a number between 1 and 10:");
+
+        while (true) {
+        int number=inValue.nextInt();
+
+        }
+    }
+}
